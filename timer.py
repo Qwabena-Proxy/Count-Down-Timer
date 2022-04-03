293ce524a19500d12799ed41a6bdefa60eb02f38
Qwabena <100248770+Qwabena-Proxy@users.noreply.github.com>
2022-04-03 21:15:35 +0000

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,41 @@
+from time import sleep as sp
+
+hrs = int(input('Hours:'))
+min = int(input('Minute:'))
+sec = int(input('Second:'))
+'''
+if len(str(hrs)) == 1:
+  hrs = '0'+(str(hrs))
+if len(str(min)) == 1:
+  min = '0'+(str(min))
+if len(str(sec)) == 1:
+  sec = '0'+(str(sec))
+secc = int(sec)
+minn = int(min)
+hrss = int(hrs)'''
+while True:
+  if len(str(hrs)) == 1:
+    h = f'0{hrs}'
+  else:
+    h = f'{hrs}'
+  if len(str(min)) == 1:
+    m = f'0{min}'
+  else:
+    m = f'{min}'
+  if len(str(sec)) == 1:
+    s = f'0{sec}'
+  else:
+    s = f'{sec}'
+  time = f'{h}:{m}:{s}'
+  print(time)
+  sec -= 1
+  sp(1) 
+  if sec == -1:
+    min -= 1
+    sec = 59
+  if min == -1 and hrs > 0:
+    hrs -= 1
+    min = 59
+  if hrs == 0 and min == 0 and sec == 0:
+    print(f'{h}:{m}:0{sec}')
+    break
